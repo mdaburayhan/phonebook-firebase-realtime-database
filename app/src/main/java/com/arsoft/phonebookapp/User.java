4bb7462f0352a5e42ab2a0361c4eae32d8d22601
Md Abu Rayhan <mdaburayhan833@gmail.com>
2025-05-31 21:00:19 +0600

    added databidning in User Model class

--- app/src/main/java/com/arsoft/phonebookapp/User.java
+++ app/src/main/java/com/arsoft/phonebookapp/User.java
@@ -1,6 +1,9 @@
 package com.arsoft.phonebookapp;
 
-public class User {
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+public class User extends BaseObservable {
     private String username;
     private String phoneNumber;
     private String groupUser;
@@ -14,27 +17,33 @@ public class User {
     public User() {
     }
 
+    @Bindable
     public String getUsername() {
         return username;
     }
 
     public void setUsername(String username) {
         this.username = username;
+        notifyPropertyChanged(BR.username);
     }
 
+    @Bindable
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
+        notifyPropertyChanged(BR.phoneNumber);
     }
 
+    @Bindable
     public String getGroupUser() {
         return groupUser;
     }
 
     public void setGroupUser(String groupUser) {
         this.groupUser = groupUser;
+        notifyPropertyChanged(BR.groupUser);
     }
 }
